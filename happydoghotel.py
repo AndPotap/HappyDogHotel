aab0e8d8cf1bdad176ba572fdf9a3a02dd4b2405
AndPotap <ap3635@columbia.edu>
2019-04-16 09:35:04 -0400

    Took out debug mode

--- happydoghotel.py
+++ happydoghotel.py
@@ -116,8 +116,7 @@ if __name__ == '__main__':
     if instance:
         # app.run()
         app.run(host=os.getenv('IP', '0.0.0.0'),
-                port=int(os.getenv('PORT', 8111)),
-                debug=True)
+                port=int(os.getenv('PORT', 8111)))
     else:
         app.run(debug=True)
 # ===========================================================================
